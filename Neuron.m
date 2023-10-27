ec4f14edbfc0bd3ecca9d7ef478c507a498be03e
rakaar <rka87338@gmail.com>
2023-10-27 16:19:43 +0530

    simplest hebb rule added

--- Neuron.m
+++ Neuron.m
@@ -8,11 +8,19 @@ classdef Neuron
         spikes 
         pre_synaptic_current
         input_current
+        xe
+        xr
+        xi
     end
 
     methods
-        function obj = update_vars(obj, v_t_minus_1, u_t_minus_1, i, ti)
+        function obj = update_vars(obj, v_t_minus_1, u_t_minus_1, i, ti, tau)
+
+            obj.input_current(ti) = i;
+            
             a=0.03; b=0.25; c=-60;  d=4; tau = 0.2;
+            tau_re = 0.9; tau_ei = 5.3; tau_ir = 800;
+
 
             v_t  = v_t_minus_1 + tau*(0.04*v_t_minus_1^2 + 5*v_t_minus_1 + 140 - u_t_minus_1 + i);
             u_t = u_t_minus_1  + tau*a*(b*v_t - u_t_minus_1);
@@ -27,7 +35,14 @@ classdef Neuron
                 obj.feedback_var(ti) = u_t;
             end
 
-        end
+            % synapse
+            M = obj.spikes(ti);
+            obj.xr(ti) = obj.xr(ti-1) + tau*(-M*obj.xr(ti-1)/tau_re + obj.xi(ti-1)/tau_ir);
+            obj.xe(ti) = obj.xe(ti-1) + tau*(M*obj.xr(ti-1)/tau_re - obj.xe(ti-1)/tau_ei);
+            obj.xi(ti) = obj.xi(ti-1) + tau*(obj.xe(ti-1)/tau_ei - obj.xi(ti-1)/tau_ir);
+            
+
+        end % update vars
     end
 
 end
--- run_model.m
+++ run_model.m
@@ -3,41 +3,67 @@ clear;
 
 % time
 tau = 0.2;  tspan = 0:tau:50;
-T1=20;
+T1=10;
 
 % Izhikevich Neuron params
 a=0.03; b=0.25; c=-60;  d=4;
 
 % n - neuron
-n = Neuron;
-n.id  = 1;
-n.voltage = zeros(1, length(tspan));
-n.spikes = zeros(1, length(tspan));
-n.feedback_var = zeros(1, length(tspan));
 
-n.voltage(1) = -64;
-n.feedback_var(1) = b*n.voltage(1);
-
+neuron_arr = cell(2, 1);
+for i = 1:2
+    neuron_arr{i,1} = Neuron;
+    neuron_arr{i,1}.id  = 1;
+    neuron_arr{i,1}.voltage = zeros(1, length(tspan));
+    neuron_arr{i,1}.spikes = zeros(1, length(tspan));
+    neuron_arr{i,1}.feedback_var = zeros(1, length(tspan));
+    
+    neuron_arr{i,1}.voltage(1) = -64;
+    neuron_arr{i,1}.feedback_var(1) = b*neuron_arr{i,1}.voltage(1);
+    
+    neuron_arr{i,1}.xr = zeros(1, length(tspan));
+    neuron_arr{i,1}.xe = zeros(1, length(tspan));
+    neuron_arr{i,1}.xi = zeros(1, length(tspan));
+    
+    neuron_arr{i,1}.xr(1) = 1;
+    
+end
+% weights initialization
+synaptic_weights = zeros(2,2, length(tspan));
+% 1 -> 2: pre, post
+synaptic_weights(1,2,1) = 1; 
+tau_synapse = 50;
 
 % input current
 input_current = zeros(1, length(tspan));
-input_current(T1:T1 + round(20/tau)) = 10;
+input_current(T1:T1 + round(30/tau)) = 10;
 
 for ti = 2:length(tspan)
-    i = input_current(ti);
+    
 
-    % update voltage
-    v_t_minus_1 = n.voltage(ti-1);
-    u_t_minus_1 = n.feedback_var(ti-1);
+    for n = 1:2
+        if n == 1
+            i = input_current(ti);
 
-    n = n.update_vars(v_t_minus_1, u_t_minus_1, i, ti);
+            v_t_minus_1 = neuron_arr{n,1}.voltage(ti-1);
+            u_t_minus_1 = neuron_arr{n,1}.feedback_var(ti-1);
+    
+            neuron_arr{n,1} = neuron_arr{n,1}.update_vars(v_t_minus_1, u_t_minus_1, i, ti, tau);
+        else
+            i = synaptic_weights(1,2)*5*neuron_arr{1,1}.xe(ti);
+            v_t_minus_1 = neuron_arr{n,1}.voltage(ti-1);
+            u_t_minus_1 = neuron_arr{n,1}.feedback_var(ti-1);
+    
+            neuron_arr{n,1} = neuron_arr{n,1}.update_vars(v_t_minus_1, u_t_minus_1, i, ti, tau);
+        end
+    end
+
+    % update synaptic weights
+    pre_syn_activity = sum(neuron_arr{1,1}.spikes(max(1,ti-50):ti-1));
+    post_syn_activity = sum(neuron_arr{2,1}.spikes(max(1,ti-50):ti-1));
+    synaptic_weights(1,2,ti) = synaptic_weights(1,2,ti - 1) +   tau*((pre_syn_activity*post_syn_activity)/tau_synapse);
+    
 end
 
 
-figure
-subplot(1,2,1)
-plot(n.spikes)
-
-subplot(1,2,2)
-plot(input_current)
 
